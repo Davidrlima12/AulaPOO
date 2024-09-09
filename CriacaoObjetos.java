342d2275713e2d263f55297ff9f044f7a3703fc3
David <127172320+Davidrlima12@users.noreply.github.com>
2024-09-09 19:43:44 -0300

    Delete CriacaoObjetos.java

--- CriacaoObjetos.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Carro carro1 = new Carro();
-    carro1.novoCarro("Toyota", "Corolla", 2020, "preto");
-    Carro carro2 = new Carro();
-    carro2.novoCarro("Honda", "Civic", 2022, "prata");
-  }
-}
